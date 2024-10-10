8611c3d8ff5e9d7555e4b607a589dd180f7c2c9f
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-10-10 15:46:42 +0300

    feat: add mailingList section

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,3 +2,4 @@ export { default as Slider } from './slider/Slider';
 export { default as PopularCategories } from './popularCategories/PopularCategories';
 export { default as LatestRecipes } from './latestRecipes/LatestRecipes';
 export { default as CurratedCollection } from './curratedCollection/CurratedCollection';
+export { default as MailingList } from './mailingList/MailingList';
---
+++ src/components/sections/mailingList/MailingList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './MailingList.module.scss';
+import Link from 'next/link';
+import { Button, EButtonClass, EButtonSize, EButtonType } from '@/components/ui';
+
+const MailingList: React.FC = () => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Deliciousness to your inbox</h2>
+        <p className={styles.description}>Enjoy weekly hand picked recipes and recommendations</p>
+        <form className={styles.form}>
+          <input type="email" placeholder=" Email Address" className={styles.input} required />
+          <Button
+            nameClass={EButtonClass.SEC}
+            size={EButtonSize.LG}
+            typeBtn={EButtonType.SUBMIT}
+            text="JOIN"
+            isLink={false}
+          />
+        </form>
+        <p className={styles.text}>
+          By joining our newsletter you agree to our <Link href="#">Terms and Conditions</Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default MailingList;
