c3f492faadbb766bfa2606a85315d0f6c08db5a9
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-10-16 08:12:14 +0300

    feat: add favorites page

--- src/components/sections/favoritesContent/FavoritesContent.tsx
+++ src/components/sections/favoritesContent/FavoritesContent.tsx
@@ -2,11 +2,38 @@ import React from 'react';
 import { recipesCards } from '@/data';
 import { LoadMoreBtn, RecipesCardsList } from '@/components/elements';
 import styles from './FavoritesContent.module.scss';
+import Image from 'next/image';
+import { Select } from '@/components/ui';
 
 const FavoritesContent: React.FC = () => {
   return (
     <section className={styles.section}>
       <div className="container">
+        <div className={styles.block}>
+          <div className={styles.titles}>
+            <div className={styles.titleBlock}>
+              <Image src={'/icons/favoritesIcon.svg'} alt="" width={44} height={44} />
+              <h1>Favorites</h1>
+            </div>
+            <Select
+              name="sort"
+              value={null}
+              options={[
+                { id: 1, name: 'Price' },
+                { id: 2, name: 'Popularity' },
+                { id: 3, name: 'Newest' },
+              ]}
+            />
+          </div>
+          <div className={styles.info}>
+            <button type="button" className={styles.btnDel}>
+              <Image src={'/icons/deleteIcon.svg'} alt="" width={20} height={20} />
+              <span>Delete all</span>
+            </button>
+            <div className={styles.count}>{`(98 recipes)`}</div>
+          </div>
+        </div>
+
         <RecipesCardsList cards={recipesCards} msg="Add recipes to favorites" />
 
         {recipesCards.length > 0 && <LoadMoreBtn />}
